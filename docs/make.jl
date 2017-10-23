bef72f3a24cc2e812a768b0d6899cb18ee66377d
huwenboshi <shihuwenbo@ucla.edu>
2017-10-23 11:26:47 -0700

    updated documentation

--- docs/make.jl
+++
@@ -1,7 +0,0 @@
-using Documenter, TraitSimulation
- 
-makedocs(modules=[TraitSimulation], doctest=true)
- 
-deploydocs(deps   = Deps.pip("mkdocs", "python-markdown-math"),
-    repo = "github.com/huwenboshi/TraitSimulation.jl.git",
-    julia  = "0.5", osname = "linux")
